847c4fe9f77a1158cee76d21dd26cf97f0bf49ff
xueyu <1375156056@qq.com>
2020-04-01 12:56:06 +0800

    初始提交

---
+++ src/main/java/com/baidu/portal/service/impl/IndexServiceImpl.java
@@ -0,0 +1,8 @@
+package com.baidu.portal.service.impl;
+
+public class IndexServiceImpl {
+
+    public void run(){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/baidu/portal/service/impl/IndexServiceImplTest.java
@@ -0,0 +1,11 @@
+package com.baidu.portal.service.impl;
+
+import static org.junit.Assert.*;
+
+public class IndexServiceImplTest {
+
+    @org.junit.Test
+    public void run() {
+        System.out.println("Hello World!");
+    }
+}
